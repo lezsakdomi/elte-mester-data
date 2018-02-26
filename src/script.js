696eb9b3cbc814450eea9a80ab75439f7e56cbad
Domonkos Lezsák <lezsakdomi1@gmail.com>
2018-02-26 23:23:07 +0100

    Remove all octokat-related code

--- src/script.js
+++ src/script.js
@@ -43,57 +43,6 @@ async function getCookies() {
 
 const getInitialCookies = getCookies();
 
-let tokenStatus = new Observable({
-    saved: false
-});
-getInitialCookies.then(value => tokenStatus.saved = value);
-async function saveToken(token) {
-    document.cookie = "ghToken=" + token;
-    tokenStatus.saved = true;
-}
-async function deleteToken() {
-    document.cookie='ghToken=;expires='+(new Date).toUTCString();
-    tokenStatus.saved = false;
-}
-
-const gatherToken = new Promise.Deferred(() => getInitialCookies.then(cookies => {
-    let token;
-    // noinspection JSUnresolvedVariable
-    if ('ghToken' in cookies) {
-        tokenStatus.saved = true;
-        // noinspection JSUnresolvedVariable
-        return cookies.ghToken;
-    } else {
-        token = prompt(
-            "Authentication required\n" +
-            "\n" +
-            "Authentication required to fetch data from GitHub. GitHub needs this to ensure nobody is trying to " +
-            "spam their service. Authenticated users can do 5000 requests per hour, but for unauthenticated " +
-            "requests the maximum is 60/hr. See https://developer.github.com/v3/#rate-limiting for more " +
-            "information.\n" +
-            "\n" +
-            "Please paste a valid GitHub personal access token below\n" +
-            "To obtain such, please visit https://github.com/settings/tokens to generate a new one. Make sure " +
-            "not to tick any special permission, because this token would be stored in an unencrypted cooke.\n" +
-            "\n" +
-            "Leave this field empty to continue anonymously\n"
-        );
-        if (token === null) throw new Error("Token is too secret for us");
-        saveToken(token);
-        return token
-    }
-}));
-
-const initOcto = gatherToken.then(token => {
-    if (token) {
-        return new Octokat({
-            token: token
-        })
-    } else {
-        return new Octokat;
-    }
-});
-
 const tsvOptions = {
     cellDelimiter: "\t",
     lineDelimiter: "\n",
@@ -103,13 +52,8 @@ function newTSV(string) {
     return new CSV(string, tsvOptions)
 }
 
-const gatherRepo = initOcto.then(octo => octo.repos("lezsakdomi", "elte-mester-data"));
-
 const baseUrl = "https://cdn.rawgit.com/lezsakdomi/elte-mester-data/master";
 
-// const fetchTemaCSV = new Promise.Deferred((value) =>
-//     gatherRepo.run(value).then(repo => repo.contents("temak.tsv").read().then(newTSV))
-// );
 const fetchTemaCSV = new Promise.Deferred((init, resolve, reject) => {
     readFile("temak.tsv")
         .then(newTSV)
@@ -150,17 +94,10 @@ function Tema(id, name, szint) {
     this.url = baseUrl + "/"+this.name;
 
     this.fetchDescription = new Promise.Deferred((init, resolve, reject) => {
-        //gatherRepo.run().then(repo => repo.contents(name+"/leiras.txt").read())
         readFile(name + "/leiras.txt").then(resolve, reject)
     }, false);
     this.description = undefined; this.fetchDescription.then(description => this.description = description);
 
-    // this.fetchFeladatList = new Promise.Deferred(() => gatherRepo.run().then(repo => repo
-    //     .contents(name+"/flist.tsv")
-    //     .read()
-    //     .then(newTSV)
-    //     .then(csv => csv.map(record => new Feladat(this, record.id, record.feladat, record.nehezseg)))
-    // ));
     this.fetchFeladatList = new Promise.Deferred((init, resolve, reject) => {
         readFile(name+"/flist.tsv")
             .then(newTSV)
